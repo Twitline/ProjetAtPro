c32d2fbc126e37cc06f1dffa3b3804a67d27b79b
Twitline <geoffreylaunay02@gmail.com>
2021-12-09 21:04:26 +0100

    experience

--- php/experience.php
+++ php/experience.php
@@ -1,1 +1,17 @@
+<?php
+$yaml=file_get_contents ("./data/experience.yaml");
+$pro=yaml_parse($yaml);
+?>
 <h1>Experiences<h1>
+<?php
+foreach ($pro as $experience) {
+    echo "<p>";
+    echo "Poste Occupé : ".$experience["Poste"]."</br>";
+    echo "Entreprise : ".$experience["Entreprise"]."</br>";
+    echo "Datedebut : ".$experience["Datedebut"]."</br>";
+    echo "Datefin : ".$experience["Datefin"]."</br>";
+    echo "Lieu : ".$experience["Lieu"]."</br>";
+    echo "Descriptif : ".$experience["Descriptif"]."</br>";
+    echo "</p>";
+}
+?>

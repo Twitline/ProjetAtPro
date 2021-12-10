682e6621f55f5bea8ac73e85e1187b3b3c4cc0cf
Geoffrey Launay <glaunay@sio.local>
2021-12-10 15:05:57 +0100

    putainsdecompetences

--- php/competence.php
+++ php/competence.php
@@ -3,10 +3,23 @@ $competence=\file_get_contents('./data/competences.yaml');
 $com=yaml_parse($competence);
 ?>
 <h1>Mes competences</h1>
-<h3>Mes competences en developpement</h3>
 <?php
-foreach($com as $item=>$niveau){
-    echo "<label for='$item'>$item</label><br>";
-    echo "<progress id='$item' max='100' value='$niveau'>$niveau</progress><br>";
+/*
+foreach($com as $domaine=>$item){
+    echo "<h3>Competences en $domaine</h3>";
+    foreach($domaine as $item=>$niveau){
+        echo "<label for='$item'>$item</label><br>";
+        echo "<progress id='$item' max='100' value='$niveau'>$niveau</progress><br>";
+    }   
+}
+*/
+
+foreach($com AS $domaine){
+    echo "<h3>".$domaine['domaine']."</h3><br>";
+    foreach($domaine["items"] as $elt){?>
+        <label for="<?=$elt['competence']?>"><?=$elt['competence']?></label><br>
+        <progress id="<?=$elt['competence']?>" max='100' value="<?=$elt['niveau']?>"><?=$elt['niveau']?></progress><br>
+        <?php
+    }
 }
 ?>

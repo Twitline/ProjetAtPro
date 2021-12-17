0f474958b9d3abb1333942e52be51eebb5432ac4
unknown <geoffreylaunay02@gmail.com>
2021-12-17 17:27:35 +0100

    formation

--- php/competence.php
+++ php/competence.php
@@ -2,12 +2,12 @@
 $competence=\file_get_contents('./data/competences.yaml');
 $com=yaml_parse($competence);
 ?>
-<h1>Mes competences</h1>
+<h1 class="centre">Mes competences</h1>
 <?php
 foreach($com AS $domaine){
-    echo "<h3>".$domaine['domaine']."</h3><br>";
+    echo "<h3 class='centre'>".$domaine['domaine']."</h3><br>";
     foreach($domaine["items"] as $elt){?>
-        <label for="<?=$elt['competence']?>"><?=$elt['competence']?></label><br>
+        <label for="<?=$elt['competence']?>"><?=$elt['competence']?><?php echo " : ".$elt['niveau']."%"?></label><br>
         <progress class="skill" id="<?=$elt['competence']?>" max='100' value="<?=$elt['niveau']?>"><?=$elt['niveau']?></progress><br>
         <?php
     }
--- php/formation.php
+++ php/formation.php
@@ -1,1 +1,17 @@
+<?php
+$formation=\file_get_contents('./data/formation.yaml');
+$form=yaml_parse($formation);
+?>
 <h1>Formation</h1>
+<?php
+foreach($form as $fan){
+    echo "<div class='formation'>";
+    echo "Formation : ".$fan["Formation"]."<br>";
+    echo "Etablissement : ".$fan["Etablissement"]."<br>";
+    echo "Date de début : ".$fan["Datedebut"]."<br>";
+    echo "Datefin : ".$fan["Datefin"]."<br>";
+    echo "Lieu : ".$fan["Lieu"]."<br>";
+    echo $fan["Contenu"]."<br>";
+    echo "</div>";
+}
+?>
